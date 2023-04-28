d8638e4622c51a907f00752b7ea398e57dfafa9d
João Victor Schiavon <schiavonj@yahoo.com.br>
2023-04-28 15:49:59 -0300

    feat: requisite 10

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,7 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
 
 
 def test_counter():
-    pass
+    "Para os parâmetros de path e word a função deve retornar a quantidade"
+    "correta de vezes que a palavra ocorre"
+    assert count_ocurrences("data/jobs.csv", "python") == 1639
